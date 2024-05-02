fca22ce67934f5ec290e2d9dfe98d4611916b090
xiaosong wen <xwen2@wpi.edu>
2024-05-01 20:19:15 -0400

    initial commit

---
+++ src/test/java/com/xiaosong/personaldrive/PersonalDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaosong.personaldrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
